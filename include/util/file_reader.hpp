b3e7ea3b42480758b30d7e4a3fb8938d340d216b
Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
2013-01-06 21:29:48 -0500

    typo fix

--- include/util/file_reader.hpp
+++ include/util/file_reader.hpp
@@ -250,7 +250,7 @@ public:
 
     typedef const iterator const_iterator;
 
-    /// get input iterator at currect read position
+    /// get input iterator at current read position
     iterator begin() { return iterator(*this); }
     const_iterator begin() const { return iterator(*this); }
 
