8544628bdad56810f313d046781da0a90d6314c1
Ghassen Hamrouni <ghamrouni@iptechinside.com>
2013-10-23 16:34:40 +0100

    Add online variance algorithm
    Add a numerically stable algorithm for the computation of the variance
    (and the mean as well).

--- include/utxx/detail/mean_variance.hpp
+++ include/utxx/detail/mean_variance.hpp
@@ -57,6 +57,24 @@ double variance(const T* begin, const T* end) {
     return sum / count;
 }
 
+// A numerically stable algorithm for the computation 
+// of the variance (and the mean as well).
+template <class T>
+double online_variance(const T* begin, const T* end) {
+	double count = 0;
+	double mean = 0;
+	double sum = 0;
+
+	for (const T* p=begin; p != end; ++p) {
+		count = count + 1;
+		double delta = *p - mean;
+		mean = mean + delta/count;
+		sum = sum + delta*(*p - mean);
+	}
+
+	return sum/count;
+}
+
 }  // namespace detail
 }  // namespace utxx
 
