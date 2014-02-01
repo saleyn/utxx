983cfaa994bf0a9844bf5e24f2c3cfc7c970909b
Serge Aleynikov <saleyn@gmail.com>
2014-02-02 02:36:45 +0400

    Work in progress

--- test/test_pmap.cpp
+++ test/test_pmap.cpp
@@ -36,76 +36,55 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <utxx/string.hpp>
 #include <limits>
 #include <stdio.h>
+#include <boost/function.hpp>
 #include <utxx/verbosity.hpp>
 #include <utxx/time_val.hpp>
+#include <utxx/endian.hpp>
 
 using namespace utxx;
+namespace bsd = boost::spirit::detail;
 
-static const unsigned char* test_set[] = {
-    "\x20",
-    "\x81",
-    "\x82\x41",
-    "\x83\x68\x7F",
-    "\x84\x6F\xAC\xEF",
-    "\x85\xEf\xDe\xCd\xBc",
-    "\x86\xEf\xDe\xCd\xBc\xAf",
-    "\x87\xEf\xDe\xCd\xBc\xAf\x9f",
-    "\x88\xEf\xDe\xCd\xBc\xAf\x9f\x8f"
+static const char* test_set[] = {
+    "\x8f\x00\x00\x00\x00\x00\x00\x00\x00",
+    "\x7e\x8f\x00\x00\x00\x00\x00\x00\x00",
+    "\x6d\x7e\x8f\x00\x00\x00\x00\x00\x00",
+    "\x5c\x6d\x7e\x8f\x00\x00\x00\x00\x00",
+    "\x4f\x5c\x6d\x7e\x8f\x00\x00\x00\x00",
+    "\x3f\x4f\x5c\x6d\x7e\x8f\x00\x00\x00",
+    "\x2f\x3f\x4f\x5c\x6d\x7e\x8f\x00\x00",
+    "\x1f\x2f\x3f\x4f\x5c\x6d\x7e\x8f\x00"
 };
 
 static void test1(bool output) {
     const uint64_t mask = 0x8080808080808080;
 
     for (int i=0; i < utxx::length(test_set); ++i) {
-        const uint64_t* v = (const uint64_t*)test_set[i];
-        uint64_t stop = *v & mask;
-        int      pos  = stop ? utxx::bits::bit_scan_reverse(stop) : -1;
+        const uint64_t v = *(const uint64_t*)test_set[i];
+        uint64_t stop = v & mask;
+        int      pos  = stop ? utxx::bits::bit_scan_forward(stop) : -1;
         if (output)
-            printf("%16lx -> %d\n", test_set[i], pos < 0 ? pos : (pos+1) >> 3);
-    }
-}
-
-BOOST_AUTO_TEST_CASE( test_pmap )
-{
-    long ITERATIONS = getenv("ITERATIONS") ? atoi(getenv("ITERATIONS")) : 10000000;
-    uint64_t value;
-
-    boost::function<void(bool)> tests[] = { &test1, &test2 };
-
-    for (int t=0; t < 2; t++)
-    {
-        time_val start = time_val::universal_time();
-
-        tests[t](verbosity::level() >= VERBOSE_DEBUG);
-
-        for (long i=0; i < ITERATIONS; ++i)
-            tests[t](false);
-
-        double elapsed = time_val::universal_time().now_diff(start);
-        int speed = (double)ITERATIONS / elapsed;
-
-        printf("Speed: %d it/s, elapsed: %.6fs\n", speed, elapsed);
+            printf("%16lx -> %d\n", v, pos < 0 ? pos : (pos+1) >> 3);
     }
 }
 
 static void test2(bool output) {
-}
-/*
-    //char acc[N];     // Accumulator
+    const int N = 8;
+    char acc[N];     // Accumulator
 
     for (int i=0; i < utxx::length(test_set); ++i) {
         int  n      = 0;
         bool done   = false;
-        for (char const*  curr = buff; curr < end && n < N && !done; ++curr, ++n)
+        for (char const* curr = test_set[i], *e=curr+8; curr < e && n < N && !done; ++curr, ++n)
         {
             char  c = *curr;
             done    = (c & 0x80);     // Check the stop bit
-            //acc[n]  = (c & 0x7f);     // Drop  the stop bit
+            acc[n]  = (c & 0x7f);     // Drop  the stop bit
         }
         if (output)
-            printf("%16lx -> %d\n", test_set[i], pos < 0 ? pos : (pos+1) >> 3);
+            printf("%16lx -> %d\n", *(const uint64_t*)test_set[i], n);
     }
-
+}
+/*
   // How many entries have we got? Their bits will be stored in the HIGH part
   // of the "PMap", and the low part filled with 0s;   initial "shift" is the
   // bit size of the "0"s padding:
@@ -125,4 +104,30 @@ static void test2(bool output) {
     (*pmap) |= tmp;
   }
 */
+
+BOOST_AUTO_TEST_CASE( test_pmap )
+{
+    long ITERATIONS = getenv("ITERATIONS") ? atol(getenv("ITERATIONS")) : 10000000;
+    uint64_t value;
+
+    BOOST_MESSAGE("Iterations: " << ITERATIONS);
+
+    boost::function<void(bool)> tests[] = { &test1, &test2 };
+
+    for (int t=0; t < 2; t++)
+    {
+        time_val start = time_val::universal_time();
+
+        tests[t](verbosity::level() >= VERBOSE_DEBUG);
+
+        for (long i=0; i < ITERATIONS; ++i)
+            tests[t](false);
+
+        double elapsed = time_val::universal_time().now_diff(start);
+        int speed = (double)ITERATIONS / elapsed;
+
+        printf("Speed: %d it/s, elapsed: %.6fs\n", speed, elapsed);
+    }
+}
+
 
