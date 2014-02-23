5fb893543dee91ac3158d0f3920bff36a7da128a
Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
2014-02-23 09:47:50 -0500

    test

--- include/utxx/multi_file_async_logger.hpp
+++ include/utxx/multi_file_async_logger.hpp
@@ -375,6 +375,9 @@ command_t {
         struct {
             bool                immediate;
         } close;
+
+        udata()  {}
+        ~udata() {}
     };
 
     const type_t        type;
@@ -385,7 +388,7 @@ command_t {
 
     command_t(const stream_info* a_si, const std::string& a_category,
               const char* a_data, size_t a_size)
-        : type(msg), stream(a_si), next(NULL), prev(NULL)
+        : command_t(msg, a_si)
     {
         set_category(a_category);
         args.msg.data.iov_base = (void*)a_data;
