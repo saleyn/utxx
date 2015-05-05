3ec65b000f8b4fcfe9ae08ca5c774d8129cd63ca
Serge Aleynikov <saleyn@gmail.com>
2015-05-05 12:13:33 -0400

    Add missing include

--- include/utxx/pcap.hpp
+++ include/utxx/pcap.hpp
@@ -36,6 +36,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <utxx/error.hpp>
 #include <utxx/endian.hpp>
 #include <boost/cstdint.hpp>
+#include <boost/assert.hpp>
 #include <boost/static_assert.hpp>
 #include <stdio.h>
 #include <arpa/inet.h>
