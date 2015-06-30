1cfd6a66cb024a753819939d2b8006cf8b240eaa
Serge Aleynikov <saleyn@gmail.com>
2015-06-30 10:12:18 -0400

    Formatting change

--- include/utxx/variant.hpp
+++ include/utxx/variant.hpp
@@ -196,7 +196,7 @@ public:
 
     bool                to_bool()   const { return boost::get<bool>(*this); }
     long                to_int()    const { return boost::get<long>(*this); }
-    long                to_float( ) const { return boost::get<double>(*this); }
+    long                to_float()  const { return boost::get<double>(*this); }
     const std::string&  to_str()    const { return boost::get<std::string>(*this); }
     const char*         c_str()     const { return boost::get<std::string>(
                                                     *this).c_str(); }
