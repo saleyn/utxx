20e04ed203f38622bee0e2ab790df815926e2ff2
Serge Aleynikov <saleyn@gmail.com>
2016-07-03 23:21:43 -0400

    Fix short_vector test errors

---
+++ include/utxx/short_vector.hpp
@@ -0,0 +1,230 @@
+//----------------------------------------------------------------------------
+/// \file   short_vector.hpp
+/// \author Serge Aleynikov
+//----------------------------------------------------------------------------
+/// \brief Generic string processing functions.
+//----------------------------------------------------------------------------
+// Copyright (c) 2016 Serge Aleynikov <saleyn@gmail.com>
+// Created: 2016-07-02
+//----------------------------------------------------------------------------
+/*
+***** BEGIN LICENSE BLOCK *****
+
+This file is part of the utxx open-source project.
+
+Copyright (C) 2016 Serge Aleynikov <saleyn@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+***** END LICENSE BLOCK *****
+*/
+#pragma once
+
+#include <type_traits>
+#include <memory>
+#include <cstring>
+#include <vector>
+#include <cassert>
+
+namespace utxx {
+
+    //--------------------------------------------------------------------------
+    /// Representation of a short vector.
+    /// @tparam T        element type
+    /// @tparam MaxItems staticly allocated buffer space to hold this many items
+    /// @tparam Alloc    custom allocator
+    /// @tparam AddItems extra spare items to allocate in calls to allocator
+    ///
+    /// Short vectors up to \a MaxItems don't involve memory allocations.
+    /// Short vector can be set to have NULL value by using "set_null()" method.
+    /// NULL vectors are represented by having size() = -1.
+    //--------------------------------------------------------------------------
+    template <typename T, int MaxItems,
+              typename Alloc = std::allocator<T>, int AddItems = 0>
+    class basic_short_vector : private Alloc {
+        using Base           = Alloc;
+
+        static_assert(sizeof(T) == sizeof(typename Alloc::value_type), "Wrong size");
+    public:
+        using value_type     = T;
+        using iterator       = T*;
+        using const_iterator = const T*;
+
+        static constexpr size_t MaxCapacity()    { return MaxItems; }
+
+        basic_short_vector(const Alloc& ac = Alloc())
+            : Base(ac), m_val(m_buf),m_sz(0),m_max_sz(MaxItems) { m_buf[0] = '\0'; }
+        basic_short_vector(const T* a, size_t n, const Alloc& ac = Alloc())
+            : Base(ac), m_val(m_buf),m_sz(0),m_max_sz(MaxItems) { set(a, n); }
+        basic_short_vector(basic_short_vector&& a, const Alloc& ac = Alloc())
+            : Base(ac)                                          { assign<false>(std::move(a));}
+        basic_short_vector(basic_short_vector const& a, const Alloc& ac = Alloc())
+            : Base(ac), m_val(m_buf),m_sz(0),m_max_sz(MaxItems) { set(a); }
+        basic_short_vector(std::vector<T> const& a, const Alloc& ac = Alloc())
+            : Base(ac), m_val(m_buf),m_sz(0),m_max_sz(MaxItems) { set(a); }
+
+        ~basic_short_vector() { deallocate(); }
+
+        void set(const std::vector<T>&     a) { set(&a[0],     a.size()); }
+        void set(const basic_short_vector& a) { set(a.begin(), a.size()); }
+        void set(const T* a, int n) {
+            assert(n >= -1);
+            if (n > int(m_max_sz))  {
+                deallocate();
+                if (n > MaxItems)
+                    std::tie(m_val, m_max_sz) = do_init_alloc(n);
+            }
+            do_copy(m_val, a, n);
+            m_sz = n;
+        }
+
+
+        /// Set to empty string without deallocating memory
+        void clear() { m_sz = 0; }
+
+        /// Deallocate memory (if allocated) and set to empty string
+        void reset() { deallocate(); clear(); }
+
+        void push_back(typename std::conditional<sizeof(T) <= sizeof(long), T, const T&>::type a) {
+            append(&a, 1);
+        }
+
+        void append(const T* a, size_t n) {
+            assert(a);
+            auto oldsz = null() ? 0 : m_sz;
+            auto sz    = oldsz+n;
+            if  (sz   <= capacity())
+                do_copy(m_val + oldsz, a, n);
+            else {
+                T* p;   int max_sz;
+                std::tie(p, max_sz) = do_init_alloc(sz);
+                do_copy(p, m_val, oldsz);
+                do_copy(p+oldsz,  a,  n);
+                deallocate();
+                m_max_sz = max_sz;
+                m_val    = p;
+            }
+            m_sz = sz;
+        }
+
+        void reserve(size_t a_capacity) {
+            if (a_capacity <= capacity())
+                return;
+
+            T* p;   int max_sz;
+            std::tie(p, max_sz) = do_init_alloc(a_capacity);
+            do_copy(p, m_val, m_sz);
+            deallocate();
+            m_max_sz = max_sz;
+            m_val    = p;
+        }
+
+        void operator= (const T*                  a) { set(a); }
+        void operator= (basic_short_vector&&      a) { assign<true>(std::move(a)); }
+        void operator= (const basic_short_vector& a) { set(a); }
+
+        bool operator==(const std::vector<T>& a) const {
+            if (m_sz != int(a.size()) || m_sz < 0) return false;
+            return do_cmp(&a[0]);
+        }
+
+        bool operator==(const basic_short_vector& a) const {
+            if (m_sz != a.m_sz) return false;
+            if (m_sz  <      0) return true;
+            return do_cmp(a.m_val);
+        }
+
+        bool operator!=(const basic_short_vector& a) const { return !operator==(a); }
+
+        typename std::conditional<sizeof(T) <= sizeof(long), T, const T&>::
+        type operator[](int n)    const { assert(n >= 0 && n < m_sz); return m_val[n]; }
+        T&   operator[](int n)          { assert(n >= 0 && n < m_sz); return m_val[n]; }
+
+        operator const T*()       const { return m_val;          }
+        const T* c_str()          const { return m_val;          }
+        T*       str()                  { return m_val;          }
+        int      size()           const { return m_sz;           }
+        void     resize(size_t n)       { assert(n < m_max_sz); m_sz = n; }
+        size_t   capacity()       const { return m_max_sz;       }
+        bool     allocated()      const { return m_val != m_buf; }
+
+        bool     null()           const { return m_sz < 0;       }
+        void     set_null()             { m_sz=-1;               }
+
+        iterator begin()                { return m_val;          }
+        iterator end()                  { return null() ? m_val : m_val+m_sz; }
+
+        const_iterator cbegin()   const { return m_val;          }
+        const_iterator cend()     const { return null() ? m_val : m_val+m_sz; }
+    private:
+        T*   m_val;
+        int  m_sz;
+        uint m_max_sz;
+        T    m_buf[MaxItems];
+
+        void deallocate() {
+            m_sz = 0;
+            if (allocated()) {
+                Base::deallocate(m_val, m_max_sz);
+                m_max_sz = MaxItems;
+                m_val    = m_buf;
+            }
+            assert(m_max_sz == MaxItems);
+        }
+
+        template <bool Reset>
+        void assign(basic_short_vector&& a) {
+            if (Reset) deallocate();
+            if (a.allocated()) { m_val = a.m_val; a.m_val = a.m_buf; }
+            else               { m_val = m_buf; do_copy(m_val, a.m_buf, a.m_sz); }
+            m_sz       = a.m_sz;
+            m_max_sz   = a.m_max_sz;
+            a.m_max_sz = MaxItems;
+            a.m_sz     = 0;
+        }
+
+        std::pair<T*,int> do_init_alloc(int n) {
+            auto max_sz = n + AddItems;
+            auto p      = Base::allocate(max_sz);
+            if (!std::is_pod<T>::value)
+                for (auto q = p, e = p+max_sz; q != e; ++q)
+                    new (q) T();
+            return std::make_pair(p, max_sz);
+        }
+
+        static void do_copy(T* a_dst, const T* a, int n) {
+            if (n > 0) {
+                assert(a);
+                if (std::is_pod<T>::value)
+                    memcpy((char*)a_dst, (char*)a, n*sizeof(T));
+                else
+                    for (auto p = a_dst, q = a, e = a+n; q != e; *p++ = *q++);
+            }
+        }
+
+        bool do_cmp(const T* q) const {
+            if (std::is_pod<T>::value)
+                return memcmp(m_val, q, m_sz) == 0;
+            else
+                for (auto p = cbegin(), e = cend(); p != e; ++p, ++q)
+                    if (*p != *q)
+                        return false;
+            return true;
+        }
+
+        static_assert(sizeof(typename Alloc::value_type) == sizeof(T), "Invalid size");
+    };
+
+} // namespace utxx
---
+++ test/test_short_vector.cpp
@@ -0,0 +1,143 @@
+//----------------------------------------------------------------------------
+/// \file  test_short_vector.cpp
+//----------------------------------------------------------------------------
+/// \brief Test cases for classes in the short_vector.hpp.
+//----------------------------------------------------------------------------
+// Copyright (c) 2010 Serge Aleynikov <saleyn@gmail.com>
+// Created: 2010-07-10
+//----------------------------------------------------------------------------
+/*
+***** BEGIN LICENSE BLOCK *****
+
+This file may be included in different open-source projects
+
+Copyright (C) 2010 Serge Aleynikov <saleyn@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+***** END LICENSE BLOCK *****
+*/
+
+#include <utxx/short_vector.hpp>
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+using namespace utxx;
+
+template <class Char = char>
+struct Alloc {
+    using value_type = Char;
+    Char* allocate  (size_t n)          { ++s_ac; ++s_count; return std::allocator<Char>().allocate(n); }
+    void  deallocate(Char* p, size_t n) { --s_count; std::allocator<Char>().deallocate(p, n); }
+
+    static long  allocations()     { return s_count; }
+    static long  tot_allocations() { return s_ac;    }
+private:
+    static long s_count;
+    static long s_ac;
+};
+
+template <class Char> long Alloc<Char>::s_count;
+template <class Char> long Alloc<Char>::s_ac;
+
+BOOST_AUTO_TEST_CASE( test_short_vector )
+{
+    using Allocator = Alloc<int>;
+    using ss = basic_short_vector<int, 16, Allocator, 1>;
+
+    Allocator salloc;
+
+    { ss s; BOOST_CHECK_EQUAL(0, s.size()); }
+    {
+        ss s(salloc);
+        BOOST_CHECK_EQUAL(0,   s.size());
+        BOOST_CHECK(!s.null());
+        BOOST_CHECK(!s.allocated());
+        BOOST_CHECK(s.begin() == s.end());
+        s.push_back(5);
+        BOOST_CHECK_EQUAL(1,   s.size());
+        BOOST_CHECK_EQUAL(5,   s[0]);
+        s[0] = 20;
+        BOOST_CHECK_EQUAL(20,  s[0]);
+        s.resize(10);
+        BOOST_CHECK_EQUAL(10,  s.size());
+        BOOST_CHECK(s.begin()+10 == s.end());
+        BOOST_CHECK(!s.null());
+        BOOST_CHECK(!s.allocated());
+
+        BOOST_CHECK_EQUAL(0, Allocator::tot_allocations());
+
+        s.reset();
+        BOOST_CHECK_EQUAL(0,   s.size());
+        BOOST_CHECK(!s.null());
+        BOOST_CHECK(!s.allocated());
+        s.set_null();
+        BOOST_CHECK(s.null());
+        BOOST_CHECK_EQUAL(-1, s.size());
+        BOOST_CHECK(s.begin()  == s.end());
+        BOOST_CHECK(s.cbegin() == s.cend());
+
+        int dummy[] = {1, 2, 3, 4};
+        s.append(dummy, 4);
+        BOOST_CHECK(!s.null());
+        BOOST_CHECK_EQUAL(4, s.size());
+        BOOST_CHECK(s.begin()+4  == s.end());
+        BOOST_CHECK(s.cbegin()+4 == s.cend());
+
+        BOOST_CHECK(!s.allocated());
+
+        s.set(nullptr, -1);
+        BOOST_CHECK(s.null());
+        BOOST_CHECK_EQUAL(-1, s.size());
+        s.push_back(5);
+        BOOST_CHECK(!s.null());
+        BOOST_CHECK_EQUAL(1, s.size());
+        BOOST_CHECK_EQUAL(5, s[0]);
+        BOOST_CHECK_EQUAL(5, s[0]);
+        BOOST_CHECK(s.begin()+1  == s.end());
+        BOOST_CHECK(s.cbegin()+1 == s.cend());
+
+        BOOST_CHECK_EQUAL(0, Allocator::tot_allocations());
+
+        const std::vector<int> test(80, 100);
+
+        s = test;                                   // <-- allocation 1
+
+        BOOST_CHECK(s.allocated());
+        BOOST_CHECK_EQUAL(1, Allocator::tot_allocations());
+        BOOST_CHECK(s == test);
+
+        BOOST_CHECK_EQUAL(1, Allocator::tot_allocations());
+        s.reset();
+        BOOST_CHECK(!s.allocated());
+        BOOST_CHECK_EQUAL(0, s.size());
+
+        BOOST_CHECK_EQUAL(1, Allocator::tot_allocations());
+        BOOST_CHECK_EQUAL(0, Allocator::allocations());
+
+        s.reserve(1000);
+
+        BOOST_CHECK(s.allocated());
+        BOOST_CHECK_EQUAL(0, s.size());
+        BOOST_CHECK_EQUAL(2, Allocator::tot_allocations());
+        BOOST_CHECK_EQUAL(1, Allocator::allocations());
+
+        s.reset();
+        BOOST_CHECK(!s.allocated());
+        BOOST_CHECK_EQUAL(0, s.size());
+        BOOST_CHECK_EQUAL(2, Allocator::tot_allocations());
+        BOOST_CHECK_EQUAL(0, Allocator::allocations());
+    }
+}
