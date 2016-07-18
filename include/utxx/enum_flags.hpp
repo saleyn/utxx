f21ddbdb7bc07c4fa2c1889621743867124c2a33
Serge Aleynikov <saleyn@gmail.com>
2016-07-18 14:13:24 -0400

    Remove deprecated UTXX_DEFINE_ENUM and UTXX_DEFINE_FLAGS
    Use supported macros instead: UTXX_ENUM, UTXX_ENUM_FLAGS, UTXX_ENUMV.
    See: [wiki](https://github.com/saleyn/utxx/wiki/Reflectable-enums).
    
    Compatibility note: enum type property `ENUM::etype` is now called `ENUM::type`.

--- include/utxx/enum_flags.hpp
+++ include/utxx/enum_flags.hpp
@@ -264,7 +264,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
             return meta(TYPE(n));                                              \
         };                                                                     \
         static const std::pair<std::string,std::string>& meta(TYPE n) {        \
-            assert(n <= s_size);                                               \
+            assert(size_t(n) <= s_size);                                       \
             return metas()[n];                                                 \
         };                                                                     \
         static constexpr type bor()       { return NONE; }                     \
@@ -279,146 +279,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
         size_t m_val;                                                          \
     }
 
-// DEPRACATED!!! Use UTXX_ENUM_FLAGS() instead!
-/// Untyped enum flags declaration:
-//  #include <utxx/enum_flags.hpp>
-//
-//  UTXX_DEFINE_FLAGS
-//  (MyEnumT,
-//      Apple,
-//      Pear,
-//      Grape,
-//      Orange
-//  );
-//
-// Sample usage:
-//  MyEnumT val = MyEnumT::from_string("Pear|Orange");
-//  std::cout << "Value: " << to_string(val) << std::endl;
-//  std::cout << "Value: " << val            << std::endl;
-//
-#define UTXX_DEFINE_FLAGS(ENUM, ...)                                        \
-    struct [[deprecated]] ENUM {                                            \
-        enum etype {                                                        \
-            NONE,                                                           \
-            BOOST_PP_SEQ_ENUM(                                              \
-                BOOST_PP_SEQ_FOR_EACH_I(                                    \
-                    UTXX_INTERNAL_FLAGS_INIT, _,                            \
-                    BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)                   \
-            )),                                                             \
-            _END_ =  1 << BOOST_PP_VARIADIC_SIZE(__VA_ARGS__),              \
-            _ALL_ = size_t(_END_) - 1                                       \
-        };                                                                  \
-                                                                            \
-    private:                                                                \
-        static const size_t s_size = BOOST_PP_VARIADIC_SIZE(__VA_ARGS__);   \
-        static const std::string* names() {                                 \
-            static const std::string s_names[] = {                          \
-                "NONE",                                                     \
-                BOOST_PP_SEQ_ENUM(                                          \
-                    BOOST_PP_SEQ_TRANSFORM(                                 \
-                        UTXX_INTERNAL_FLAGS_STRINGIFY, ,                    \
-                        BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)               \
-                ))                                                          \
-            };                                                              \
-            return s_names;                                                 \
-        };                                                                  \
-                                                                            \
-        static const std::string& name(size_t n) {                          \
-            assert(n <= s_size);                                            \
-            return names()[n];                                              \
-        };                                                                  \
-        static constexpr etype bor()        { return NONE; }                \
-        static constexpr etype bor(etype a) { return a;    }                \
-                                                                            \
-        template <typename... Args>                                         \
-        static constexpr etype bor(etype a, Args... args)                   \
-        { return etype(size_t(a) | size_t(bor(args...))); }                 \
-                                                                            \
-        UTXX__ENUM_FRIEND_SERIALIZATION__;                                  \
-                                                                            \
-        size_t m_val;                                                       \
-                                                                            \
-    public:                                                                 \
-        constexpr ENUM()                  : m_val(0)          {}            \
-        explicit constexpr ENUM(etype v)  : m_val(size_t(v))  {}            \
-        template <typename... Args>                                         \
-        constexpr ENUM(etype a, Args... args) : m_val(bor(a, args...)) {}   \
-                                                                            \
-        ENUM(ENUM&&)                 = default;                             \
-        ENUM(ENUM const&)            = default;                             \
-                                                                            \
-        ENUM& operator=(ENUM const&) = default;                             \
-        ENUM& operator=(ENUM&&)      = default;                             \
-                                                                            \
-        void               clear()             { m_val = 0;     }           \
-        void               clear(etype  a)     { m_val &= ~size_t(a); }     \
-        void               clear(size_t a)     { m_val &= ~a;   }           \
-        constexpr operator size_t()      const { return m_val;  }           \
-        constexpr operator uint()        const { return m_val;  }           \
-        constexpr operator etype()       const { return etype(m_val); }     \
-        constexpr bool     empty()       const { return !m_val; }           \
-        constexpr bool     has(etype  a) const { return m_val & size_t(a); }\
-        constexpr bool has_any(size_t a) const { return m_val & a;         }\
-        constexpr bool has_all(size_t a) const { return (m_val & a) == a;  }\
-        constexpr bool   valid(size_t a) const { return a < _END_;    }     \
-        static constexpr size_t size()         { return s_size;       }     \
-                                                                            \
-        ENUM operator=  (etype  a) { m_val  = size_t(a); return *this; }    \
-        ENUM operator=  (size_t a) { m_val  = a;         return *this; }    \
-        ENUM operator|= (etype  a) { m_val |= size_t(a); return *this; }    \
-                                                                            \
-        friend constexpr ENUM operator| (ENUM a, ENUM b)  {                 \
-            return ENUM(etype(a.m_val | b.m_val));                          \
-        }                                                                   \
-        std::string to_string() const {                                     \
-            std::stringstream s;                                            \
-            for (size_t i=0; i < s_size; ++i)                               \
-                if (m_val & (1ul << i))                                     \
-                    s << (s.rdbuf()->in_avail() ? "|" : "") << name(i+1);   \
-            return s.str();                                                 \
-        }                                                                   \
-        static std::string to_string(etype a){ return ENUM(a).to_string();} \
-                                                                            \
-        static ENUM from_string(const std::string& a, bool a_nocase=false){ \
-            return from_string(a.c_str(), a_nocase);                        \
-        }                                                                   \
-                                                                            \
-        static ENUM from_string(const char* a, bool a_nocase=false){        \
-            static const auto s_sep = boost::is_any_of("|,; ");             \
-            auto f = a_nocase ? &strcasecmp : &strcmp;                      \
-            std::vector<std::string> v; size_t val = 0;                     \
-            boost::split(v, a, s_sep, boost::token_compress_on);            \
-            for (auto& s : v) {                                             \
-                size_t n = 0;                                               \
-                for (size_t i=0; i < s_size; i++)                           \
-                    if (f((names()[i+1]).c_str(), s.c_str()) == 0) {        \
-                        n |= (1ul << i);                                    \
-                        break;                                              \
-                    }                                                       \
-                if (!n)                                                     \
-                    UTXX_THROW_BADARG_ERROR("Invalid flag value: ", s);     \
-                val |= n;                                                   \
-            }                                                               \
-            return etype(val);                                              \
-        }                                                                   \
-                                                                            \
-        inline friend std::ostream& operator<< (std::ostream& out, ENUM a) {\
-            return out << ENUM::to_string(a);                               \
-        }                                                                   \
-                                                                            \
-        template <typename Visitor>                                         \
-        void for_each(const Visitor& a_fun) {                               \
-            for (size_t i=0; i < s_size; i++) {                             \
-                etype v = etype(1ul << i);                                  \
-                if ((m_val & v) && !a_fun(v))                               \
-                    break;                                                  \
-            }                                                               \
-        }                                                                   \
-    }
-
 // Internal macro for supporting BOOST_PP_SEQ_TRANSFORM
-#define UTXX_INTERNAL_FLAGS_STRINGIFY(x, _, item) BOOST_PP_STRINGIZE(item)
-#define UTXX_INTERNAL_FLAGS_INIT(x, _, i, item)   (item = 1ul << i)
 #define UTXX_INTERNAL_FLAGZ_INIT(x, _, i, item)                                \
             (BOOST_PP_TUPLE_ELEM(0, item) = 1ul << i)
 
--- test/test_enum.cpp
+++ test/test_enum.cpp
@@ -64,39 +64,6 @@ UTXX_ENUM_FLAGZ(mm_flagz, uint8_t, Nil,
 // Define an enum my_enum2 inside a struct:
 struct oh_mm { UTXX_ENUM(mm_enum2, char, X, Y); };
 
-//------------------------------------------------------------------------------
-// Using deprecated UTXX_DEFINE_ENUM, UTXX_DEFINE_ENUMX, UTXX_DEFINE_FLAGS
-//------------------------------------------------------------------------------
-UTXX_DEFINE_ENUMX
-(my_enumx0, ' ',
-    (A,  'a')
-    (BB, 'b')
-    (CCC)
-);
-
-UTXX_DEFINE_ENUMX(my_enumx, ' ', (A, 'a')(BB, 'b')(CCC));
-
-UTXX_DEFINE_ENUMX(SideT, -1, (BID)(ASK)(SIDES));
-
-// Define an enum with values A, B, C that can be converted to string
-// and fron string using reflection class:
-UTXX_DEFINE_ENUM(my_enum,
-    A,  // Comment A
-    B,
-    C   // Comment C
-);
-
-UTXX_DEFINE_FLAGS(my_flags,
-    A,
-    B,
-    C,
-    D,
-    E
-);
-
-// Define an enum my_enum2 inside a struct:
-struct oh_my { UTXX_DEFINE_ENUM(my_enum2, X, Y); };
-
 BOOST_AUTO_TEST_CASE( test_enum )
 {
     static_assert(3 == mm_enum::size(),         "Invalid size");
@@ -164,55 +131,7 @@ BOOST_AUTO_TEST_CASE( test_enum )
     BOOST_CHECK(oh_mm::mm_enum2::UNDEFINED == oh_mm::mm_enum2::from_string("D"));
 }
 
-BOOST_AUTO_TEST_CASE( test_enum_old )
-{
-    static_assert(3 == my_enum::size(), "Invalid size");
-
-    my_enum v;
-
-    BOOST_CHECK(v.empty());
-
-    BOOST_CHECK_EQUAL(0,               (int)my_enum::UNDEFINED);
-    BOOST_CHECK_EQUAL(my_enum::UNDEFINED,   my_enum(0));
-    BOOST_CHECK_EQUAL(my_enum::A,           my_enum(1));
-    BOOST_CHECK_EQUAL(my_enum::A,           my_enum::begin());
-    BOOST_CHECK_EQUAL(my_enum::C,           my_enum::last());
-    BOOST_CHECK_EQUAL(my_enum::_END_,       my_enum::end());
-    BOOST_CHECK_EQUAL(1+int(my_enum::C),    my_enum::end());
-    BOOST_CHECK_EQUAL("A", my_enum::to_string(my_enum::A));
-    BOOST_CHECK_EQUAL("B", my_enum::to_string(my_enum::B));
-    BOOST_CHECK_EQUAL("C", my_enum::to_string(my_enum::C));
-    BOOST_CHECK_EQUAL("A", my_enum::from_string("A").to_string());
-
-    {
-        my_enum val = my_enum::from_string("B");
-        BOOST_CHECK_EQUAL("B", val.to_string());
-        std::stringstream s; s << my_enum::to_string(val);
-        BOOST_CHECK_EQUAL("B", s.str());
-    }
-
-    {
-        // Iterate over all enum values defined in my_enum type:
-        std::stringstream s;
-        my_enum::for_each([&s](my_enum e) { s << e; return true; });
-        BOOST_CHECK_EQUAL("ABC", s.str());
-    }
-
-    BOOST_CHECK(my_enum::A == my_enum::from_string("A"));
-    BOOST_CHECK(my_enum::B == my_enum::from_string("B"));
-    BOOST_CHECK(my_enum::C == my_enum::from_string("C"));
-    BOOST_CHECK(my_enum::UNDEFINED == my_enum::from_string("D"));
-
-    static_assert(2 == oh_my::my_enum2::size(), "Invalid size");
-    BOOST_CHECK_EQUAL("X", oh_my::my_enum2::to_string(oh_my::my_enum2::X));
-    BOOST_CHECK_EQUAL("Y", oh_my::my_enum2::to_string(oh_my::my_enum2::Y));
-
-    BOOST_CHECK(oh_my::my_enum2::X == oh_my::my_enum2::from_string("X"));
-    BOOST_CHECK(oh_my::my_enum2::Y == oh_my::my_enum2::from_string("Y"));
-    BOOST_CHECK(oh_my::my_enum2::UNDEFINED == oh_my::my_enum2::from_string("D"));
-}
-
-BOOST_AUTO_TEST_CASE( test_enumx )
+BOOST_AUTO_TEST_CASE( test_enumv )
 {
     static_assert(3 == mm_enumv::size(), "Invalid size");
 
@@ -287,48 +206,6 @@ BOOST_AUTO_TEST_CASE( test_enumx )
     BOOST_CHECK(mm_enumv::UNDEFINED == mm_enumv::from_string("D"));
 
     static_assert(3 == mm_enumv::size(), "Invalid size");
-}
-
-BOOST_AUTO_TEST_CASE( test_enumx_old )
-{
-    static_assert(3 == my_enumx::size(), "Invalid size");
-
-    my_enumx v;
-
-    BOOST_CHECK(v.empty());
-
-    BOOST_CHECK_EQUAL(' ',     (char)my_enumx::UNDEFINED);
-    BOOST_CHECK_EQUAL(my_enumx::A,   my_enumx('a'));
-    BOOST_CHECK_EQUAL(my_enumx::BB,  my_enumx('b'));
-    BOOST_CHECK_EQUAL(my_enumx::CCC, my_enumx('c'));
-    BOOST_CHECK_EQUAL("A",           my_enumx::to_string(my_enumx::A));
-    BOOST_CHECK_EQUAL("BB",          my_enumx::to_string(my_enumx::BB));
-    BOOST_CHECK_EQUAL("CCC",         my_enumx::to_string(my_enumx::CCC));
-    BOOST_CHECK_EQUAL("A",           my_enumx::from_string("A").to_string());
-
-    {
-        my_enumx val = my_enumx::from_string("BB");
-        BOOST_CHECK_EQUAL("BB", val.to_string());
-        std::stringstream s; s << my_enumx::to_string(val);
-        BOOST_CHECK_EQUAL("BB", s.str());
-    }
-
-    {
-        // Iterate over all enum values defined in my_enum type:
-        std::stringstream s;
-        my_enumx::for_each([&s](my_enumx e) {
-            s << e;
-            return true;
-        });
-        BOOST_CHECK_EQUAL("ABBCCC", s.str());
-    }
-
-    BOOST_CHECK(my_enumx::A   == my_enumx::from_string("A"));
-    BOOST_CHECK(my_enumx::BB  == my_enumx::from_string("BB"));
-    BOOST_CHECK(my_enumx::CCC == my_enumx::from_string("CCC"));
-    BOOST_CHECK(my_enumx::UNDEFINED == my_enumx::from_string("D"));
-
-    static_assert(3 == my_enumx::size(), "Invalid size");
 }
 
 BOOST_AUTO_TEST_CASE( test_enum_flags )
@@ -415,63 +292,6 @@ BOOST_AUTO_TEST_CASE( test_enum_flags )
         std::stringstream s; s << mm_flags::to_string(val);
         BOOST_CHECK_EQUAL("A|B|E", s.str());
     }
-}
-
-BOOST_AUTO_TEST_CASE( test_enum_flags_old )
-{
-    static_assert(5 == my_flags::size(), "Invalid size");
-
-    my_flags v;
-
-    BOOST_CHECK(v.empty());
-    BOOST_CHECK(v == my_flags::NONE);
-
-    v |= my_flags::B;
-
-    BOOST_CHECK_EQUAL(0,        my_flags::NONE);
-    BOOST_CHECK_EQUAL(1u << 0,  my_flags::A);
-    BOOST_CHECK_EQUAL(1u << 1,  my_flags::B);
-    BOOST_CHECK_EQUAL("A",      my_flags(my_flags::A).to_string());
-    BOOST_CHECK_EQUAL("B",      my_flags(my_flags::B).to_string());
-    BOOST_CHECK_EQUAL("A|C",    my_flags(my_flags::A, my_flags::C).to_string());
-    BOOST_CHECK_EQUAL("A",      my_flags::from_string("A").to_string());
-
-    BOOST_CHECK_THROW(my_flags::from_string("A|F"), utxx::badarg_error);
-
-    {
-        my_flags val = my_flags::from_string("A|B|E");
-        BOOST_CHECK_EQUAL("A|B|E", val.to_string());
-        std::stringstream s; s << my_flags::to_string(val);
-        BOOST_CHECK_EQUAL("A|B|E", s.str());
-    }
-
-    v |= my_flags::E;
-
-    BOOST_CHECK( v.has_all(my_flags::B | my_flags::E));
-    BOOST_CHECK(!v.has_all(my_flags::A | my_flags::B | my_flags::E));
-    BOOST_CHECK( v.has_all(my_flags::B));
-    BOOST_CHECK( v.has_all(my_flags::E));
-    BOOST_CHECK( v.has_any(my_flags::B | my_flags::E));
-    BOOST_CHECK( v.has_any(my_flags::B));
-    BOOST_CHECK( v.has_any(my_flags::E));
-    BOOST_CHECK( v.has(my_flags::B));
-    BOOST_CHECK( v.has(my_flags::E));
-
-    {
-        // Iterate over all enum values defined in my_enum type:
-        std::stringstream s;
-        v.for_each([&s](my_flags e) {
-            s << e;
-            return true;
-        });
-        BOOST_CHECK_EQUAL("BE", s.str());
-    }
-
-    v.clear();
-    BOOST_CHECK(v.empty());
-    v = my_flags::B | my_flags::C | my_flags::E;
-    v.clear(my_flags::C | my_flags::E);
-    BOOST_CHECK(v == my_flags::B);
 }
 
 #endif
