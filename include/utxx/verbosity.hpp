250309339d5a09aeb4ebc7513f126c9cbbeeead1
Serge Aleynikov <saleyn@gmail.com>
2017-01-28 08:06:05 -0500

    Update verbosity.hpp

--- include/utxx/verbosity.hpp
+++ include/utxx/verbosity.hpp
@@ -33,8 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 
-#ifndef _UTXX_VERBOSITY_HPP_
-#define _UTXX_VERBOSITY_HPP_
+#pragma once
 
 #include <stdlib.h>
 #include <utxx/compiler_hints.hpp>
@@ -107,6 +106,3 @@ public:
 };
 
 } // namespace utxx
-
-#endif // _UTXX_VERBOSITY_HPP_
-
