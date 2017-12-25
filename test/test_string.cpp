2d93dab0aca17444b1067694e91ee955ed826284
Serge Aleynikov <saleyn@gmail.com>
2017-12-25 11:05:59 -0500

    Add test case

--- test/test_string.cpp
+++ test/test_string.cpp
@@ -186,6 +186,7 @@ BOOST_AUTO_TEST_CASE( test_string_wildcard )
     TEST_WILDCARD("a12b12", "*12*12*", true);
     TEST_WILDCARD("some same crazy address address",          "*address",  true);
     TEST_WILDCARD("some same crazy address address",          "*address*", true);
+    TEST_WILDCARD("some same crazy address address!",         "*address", false);
     TEST_WILDCARD("some same crazy address address\naddress", "*address",  true);
     TEST_WILDCARD("some same crazy address address\nAddress", "*address", false);
     TEST_WILDCARD("some same crazy address address\nAddress", "*address*", true);
