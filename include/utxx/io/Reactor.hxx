e53dbac164a0dc63edd9f1d2fe6bda26d63e3ed1
Serge Aleynikov <saleyn@gmail.com>
2020-09-02 11:30:42 -0400

    Fix reactor references

--- include/utxx/io/Reactor.hxx
+++ include/utxx/io/Reactor.hxx
@@ -32,6 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #pragma once
 
+#include <utxx/io/ReactorLog.hpp>
 #include <utxx/io/Reactor.hpp>
 #include <utxx/timestamp.hpp>
 
