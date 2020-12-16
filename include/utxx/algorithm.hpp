5bd9a31054dd748dda752a46f31bf32e497354e8
Serge Aleynikov <saleyn@gmail.com>
2020-12-16 16:21:33 -0500

    Add container search for closest element

---
+++ include/utxx/algorithm.hpp
@@ -0,0 +1,71 @@
+//------------------------------------------------------------------------------
+/// \file   algorithm.hpp
+/// \author Serge Aleynikov
+//------------------------------------------------------------------------------
+/// \brief This file defines various container-based algorithms
+//------------------------------------------------------------------------------
+// Copyright (c) 2020 Serge Aleynikov <saleyn@gmail.com>
+// Created: 2020-12-10
+//------------------------------------------------------------------------------
+/*
+***** BEGIN LICENSE BLOCK *****
+
+This file is part of the utxx open-source project.
+
+Copyright (C) 2016 Serge Aleynikov <saleyn@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+***** END LICENSE BLOCK *****
+*/
+#pragma once
+
+#include <iterator>
+
+namespace utxx {
+
+template <typename Set>
+typename Set::const_iterator
+find_closest(Set const & s, typename Set::value_type const & val)
+{
+    auto a = s.begin(), b = s.end(), it = s.lower_bound(val);
+
+    if (it == b)
+    {
+        if (it != a) --it;
+        return it;
+    }
+
+    auto nt = std::next(it);
+
+    if (nt == b) return it;
+    return val - *it < *nt - val ? it : nt;
+}
+
+template <typename Set>
+typename Set::const_iterator
+find_ge(Set const & s, typename Set::value_type const & val)
+{
+    auto it = s.upper_bound(val);
+
+    if (it == s.begin())
+      return it;
+
+    auto pr = std::prev(it);
+
+    return *pr == val ? pr : it;
+}
+
+} // namespace utxx
---
+++ test/test_algorithm.cpp
@@ -0,0 +1,71 @@
+//----------------------------------------------------------------------------
+/// \file  test_algorithm.cpp
+//----------------------------------------------------------------------------
+/// \brief This is a test file for validating algorithm.hpp functionality.
+//----------------------------------------------------------------------------
+// Copyright (c) 2011 Serge Aleynikov <saleyn@gmail.com>
+// Created: 2020-12-10
+//----------------------------------------------------------------------------
+/*
+***** BEGIN LICENSE BLOCK *****
+
+This file may be included in different open-source projects.
+
+Copyright (C) 2011 Serge Aleynikov <saleyn@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+***** END LICENSE BLOCK *****
+*/
+
+#include <boost/test/unit_test.hpp>
+#include <utxx/algorithm.hpp>
+#include <set>
+
+namespace utxx {
+
+BOOST_AUTO_TEST_CASE(test_algorithm_find_closest)
+{
+    std::set<int> set{ 3, 4, 7, 9 };
+
+    auto x = find_closest(set, 1); //Returns '3'
+    auto a = find_closest(set, 6); //Returns '7'
+    auto b = find_closest(set, 4); //Returns '4'
+    auto c = find_closest(set, 10); //Returns '9'
+
+    BOOST_REQUIRE_EQUAL(3, *x);
+    BOOST_REQUIRE_EQUAL(7, *a);
+    BOOST_REQUIRE_EQUAL(4, *b);
+    BOOST_REQUIRE_EQUAL(9, *c);
+}
+
+BOOST_AUTO_TEST_CASE(test_algorithm_find_ge)
+{
+    std::set<int> set{ 3, 4, 7, 9 };
+
+    auto x = find_ge(set, 1); //Returns '3'
+    auto a = find_ge(set, 6); //Returns '7'
+    auto b = find_ge(set, 4); //Returns '4'
+    auto c = find_ge(set, 8); //Returns '9'
+    auto d = find_ge(set, 10); //Returns 'end'
+
+    BOOST_REQUIRE_EQUAL(3, *x);
+    BOOST_REQUIRE_EQUAL(7, *a);
+    BOOST_REQUIRE_EQUAL(4, *b);
+    BOOST_REQUIRE_EQUAL(9, *c);
+    BOOST_REQUIRE(d == set.end());
+}
+
+} // namespace utxx
